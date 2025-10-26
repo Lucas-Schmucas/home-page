e6c103f20a4c138034a3b0ddd4c60170cab9ca53
lucas-schmucas <l.breiner@clipmyhorse.tv>
2025-10-26 20:41:21 +0100

    setup service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
+        Model::unguard();
+
+        DB::prohibitDestructiveCommands(app()->isProduction());
+
+        Date::use(CarbonImmutable::class);
+
+        URL::forceHttps(app()->isProduction() || app()->environment('stage'));    }
 }
