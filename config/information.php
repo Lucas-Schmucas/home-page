695c536510a6a3bc8d2dc93c3f6ddcb9c7955822
lucas-schmucas <l.breiner@clipmyhorse.tv>
2025-11-27 23:50:55 +0100

    clean up

--- config/information.php
+++ config/information.php
@@ -4,4 +4,8 @@ return [
     'contact' => [
         'mail' => env('CONTACT_MAIL'),
     ],
+    'social' => [
+        'github' => env('SOCIAL_GITHUB'),
+        'xing' => env('SOCIAL_XING'),
+    ],
 ];
