9782d2d5000a6784851167e47b6d22a0fbb113b3
lucas-schmucas <l.breiner@clipmyhorse.tv>
2026-05-25 20:42:35 +0200

    update worker config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,12 @@ namespace App\Providers;
 
 use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Queue\Events\Looping;
+use Illuminate\Queue\Events\WorkerStopping;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -33,5 +37,8 @@ class AppServiceProvider extends ServiceProvider
         Date::use(CarbonImmutable::class);
 
         URL::forceHttps(app()->isProduction() || app()->environment('stage'));
+
+        Event::listen(Looping::class, fn () => Log::warning('[queue-debug] Looping iteration', ['pid' => getmypid(), 'mem_mb' => round(memory_get_usage(true) / 1048576, 1)]));
+        Event::listen(WorkerStopping::class, fn (WorkerStopping $e) => Log::warning('[queue-debug] WorkerStopping', ['pid' => getmypid(), 'status' => $e->status, 'mem_mb' => round(memory_get_usage(true) / 1048576, 1)]));
     }
 }
